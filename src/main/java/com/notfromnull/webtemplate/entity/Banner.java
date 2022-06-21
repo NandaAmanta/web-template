9fa66bed0b2f3e66061951e8d337016a061d854d
Nanda Amanta <nansamgr@gmail.com>
2022-06-21 08:02:06 +0800

    Update Banner.java

--- src/main/java/com/notfromnull/webtemplate/entity/Banner.java
+++ src/main/java/com/notfromnull/webtemplate/entity/Banner.java
@@ -4,7 +4,6 @@
  */
 package com.notfromnull.webtemplate.entity;
 
-import java.util.List;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -12,66 +11,5 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * @author ASUS
  */
 @Document(collection = "Banner")
-public class Banner {
-
-    /**
-     * @return the html
-     */
-    public String getHtml() {
-        return html;
-    }
-
-    /**
-     * @param html the html to set
-     */
-    public void setHtml(String html) {
-        this.html = html;
-    }
-
-    /**
-     * @return the css
-     */
-    public String getCss() {
-        return css;
-    }
-
-    /**
-     * @param css the css to set
-     */
-    public void setCss(String css) {
-        this.css = css;
-    }
-
-    /**
-     * @return the images
-     */
-    public List<String> getImages() {
-        return images;
-    }
-
-    /**
-     * @param images the images to set
-     */
-    public void setImages(List<String> images) {
-        this.images = images;
-    }
-
-    /**
-     * @return the js
-     */
-    public String getJs() {
-        return js;
-    }
-
-    /**
-     * @param js the js to set
-     */
-    public void setJs(String js) {
-        this.js = js;
-    }
-
-    private String html;
-    private String css;
-    private List<String> images;
-    private String js;
+public class Banner extends WebTemplate {
 }
